66258063fd79f3334cf169998d70cd4be750f847
David1367 <pedro.571.david@gmail.com>
2016-07-20 08:04:19 -0400

    Added put, patch and delete operations

--- DBSP.ODataV4.AirVinyl.API/Controllers/PeopleController.cs
+++ DBSP.ODataV4.AirVinyl.API/Controllers/PeopleController.cs
@@ -126,6 +126,85 @@ namespace DBSP.ODataV4.AirVinyl.API.Controllers
       return Created(person);
     }
 
+    public IHttpActionResult Put([FromODataUri] int key, Person person)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      var currentPerson = _ctx.People.FirstOrDefault(p => p.PersonId == key);
+      if (currentPerson == null)
+      {
+        return NotFound();
+      }
+
+      // Alternative: If the person isn't found: Uperset. This must only
+      // be used if the responsibility for creating the key isn't at 
+      // server-level. in our case, we're using auto-increment fields,
+      // so this isn't allowed - code is for illustration purposes only!
+      //if (currentPerson == null)
+      //{
+      //  // the key from the URI is the key we should use
+      //  person.PersonId = key;
+      //  _ctx.People.Add(person);
+      //  _ctx.SaveChanges();
+      //  return Created(person);
+      //}
+
+      person.PersonId = currentPerson.PersonId;
+      _ctx.Entry(currentPerson).CurrentValues.SetValues(person);
+      _ctx.SaveChanges();
+
+      return StatusCode(HttpStatusCode.NoContent);
+    }
+
+    public IHttpActionResult Patch([FromODataUri] int key, Delta<Person> patch)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      var currentPerson = _ctx.People.FirstOrDefault(p => p.PersonId == key);
+      if (currentPerson == null)
+      {
+        return NotFound();
+      }
+
+      //patch.Patch(currentPerson);
+      //_ctx.SaveChanges();
+
+      // Adding a workaround
+      var id = currentPerson.PersonId;
+      patch.Patch(currentPerson);
+      currentPerson.PersonId = id;
+      _ctx.SaveChanges();
+
+      return StatusCode(HttpStatusCode.NoContent);
+    }
+
+    public IHttpActionResult Delete([FromODataUri] int key)
+    {
+      var currentPerson = _ctx.People.Include("Friends").FirstOrDefault(p => p.PersonId == key);
+      if (currentPerson == null)
+      {
+        return NotFound();
+      }
+
+      var peopleWithCurrentPersonAsFriend = _ctx.People.Include("Friends")
+          .Where(p => p.Friends.Select(f => f.PersonId).AsQueryable().Contains(key));
+      foreach (var person in peopleWithCurrentPersonAsFriend.ToList())
+      {
+        person.Friends.Remove(currentPerson);
+      }
+
+      _ctx.People.Remove(currentPerson);
+      _ctx.SaveChanges();
+
+      return StatusCode(HttpStatusCode.NoContent);
+    }
+
     protected override void Dispose(bool disposing)
     {
       _ctx.Dispose();
