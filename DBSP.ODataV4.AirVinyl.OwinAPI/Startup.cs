5c48a92ed94077635ff7884f13d293be5130f323
David1367 <pedro.571.david@gmail.com>
2016-08-27 10:33:58 -0400

    Fixed app.config and web.config in the projects' solution

--- DBSP.ODataV4.AirVinyl.OwinAPI/Startup.cs
+++ DBSP.ODataV4.AirVinyl.OwinAPI/Startup.cs
@@ -33,6 +33,19 @@ namespace DBSP.ODataV4.AirVinyl.OwinAPI
       return config;
     }
 
+    //private static IEdmModel GetEdmModel()
+    //{
+    //  var builder = new ODataConventionModelBuilder();
+    //  builder.Namespace = "AirVinyl";
+    //  builder.ContainerName = "AirVinylContainer";
+
+    //  builder.EntitySet<Person>("People");
+    //  builder.EntitySet<VinylRecord>("VinylRecords");
+    //  builder.EntitySet<RecordStore>("RecordStores");
+
+    //  return builder.GetEdmModel();
+    //}
+
     private static IEdmModel GetEdmModel()
     {
       var builder = new ODataConventionModelBuilder();
@@ -40,8 +53,46 @@ namespace DBSP.ODataV4.AirVinyl.OwinAPI
       builder.ContainerName = "AirVinylContainer";
 
       builder.EntitySet<Person>("People");
-      builder.EntitySet<VinylRecord>("VinylRecords");
       builder.EntitySet<RecordStore>("RecordStores");
+
+      // function bound to RecordStore entity
+      var isHighRatedFunction = builder.EntityType<RecordStore>().Function("IsHighRated");
+      isHighRatedFunction.Returns<bool>();
+      isHighRatedFunction.Parameter<int>("minimumRating");
+      isHighRatedFunction.Namespace = "AirVinyl.Functions";
+
+      // function bound to RecordStore list
+      var areRatedByFunction = builder.EntityType<RecordStore>().Collection.Function("AreRatedBy");
+      areRatedByFunction.ReturnsCollectionFromEntitySet<RecordStore>("RecordStores");
+      areRatedByFunction.CollectionParameter<int>("personIds");
+      areRatedByFunction.Namespace = "AirVinyl.Functions";
+
+      // unbound function(no entity type)
+      var getHighRatedRecordStoresFunction = builder.Function("GetHighRatedRecordStores");
+      getHighRatedRecordStoresFunction.Parameter<int>("minimumRating");
+      getHighRatedRecordStoresFunction.ReturnsCollectionFromEntitySet<RecordStore>("RecordStores");
+      getHighRatedRecordStoresFunction.Namespace = "AirVinyl.Functions";
+
+
+      // action bound to RecordStore entity
+      var rateAction = builder.EntityType<RecordStore>().Action("Rate");
+      rateAction.Returns<bool>();
+      rateAction.Parameter<int>("rating");
+      rateAction.Parameter<int>("personId");
+      rateAction.Namespace = "AirVinyl.Actions";
+
+      // action bound to RecordStore collection
+      var removeRatingsAction = builder.EntityType<RecordStore>().Collection.Action("RemoveRatings");
+      removeRatingsAction.Returns<bool>();
+      removeRatingsAction.Parameter<int>("personId");
+      removeRatingsAction.Namespace = "AirVinyl.Actions";
+
+      // unbound action
+      var removeRecordStoreRatingsAction = builder.Action("RemoveRecordStoreRatings");
+      removeRecordStoreRatingsAction.Parameter<int>("personId");
+      removeRecordStoreRatingsAction.Namespace = "AirVinyl.Actions";
+
+      builder.Singleton<Person>("Kenneth");
 
       return builder.GetEdmModel();
     }
